f4f61f4b5b06ad614bb8547dbdc166253e0ab507
binayaluitel2 <binayababuluitel@gmail.com>
2021-03-31 23:16:53 -0400

    deploying

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,18 +1,18 @@
-// const mongoose = require("mongoose");
-// require("dotenv").config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-// const dbConfig = process.env.MONGODB_URI || "mongodb://localhost/workout";
+const dbConfig = process.env.MONGODB_URI || "mongodb://localhost/workout";
 
-// async function connectDB() {
-//     await mongoose.connect(
-//         dbConfig, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//             useCreateIndex: true,
-//             useFindAndModify: false
-//         },
-//         () => console.log("Connected to DB")
-//     );
-// }
+async function connectDB() {
+    await mongoose.connect(
+        dbConfig, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        },
+        () => console.log("Connected to DB")
+    );
+}
 
-// module.exports = connectDB;
+module.exports = connectDB;
